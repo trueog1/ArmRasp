4a10b507f9965faf4f635733db78c33b15e56c5b
Songbird14 <leah.hanen.5571@gmail.com>
2025-03-11 13:35:20 -0700

    add code for the notes

--- Functions/piano_playing.py
+++ Functions/piano_playing.py
@@ -166,6 +166,36 @@ class Perception():
             self.center_locations = []
             self.previous_time = time.time()
 
+    # Notes from sheet music
+    def get_music(self):
+        pre_chorous= 'd c d d d d d d c d e d d d d d e d d d d a g f e f e f e f f f d e e e e d g f '
+        chorous = 'f f d s d d g f d d e e d f d e d e f e d e e d f d e d e d f e d e d f e d d e f e f e f e f f e d'
+        pre_chorous_notes= pre_chorous.split(' ')
+        chorous_notes = chorous.split(' ')
+
+        #types of notes from sheet music
+        pre_chorous_type = [8, 8 ,8, 8, 8, 4, 8, 8, 8, 4, 4, 8, 8, 8, 8, 8, 4, 8, 8, 8, 8, 8, 8, 8, 4, 8, 8, 8, 8, 8, 8, 8, 4, 4, 8, 8, 8, 4, 4, 4, 2 ]
+        chorous_type = [8, 4, 8, 4, 8, 8, 4, 4, 4, 8, 8, 8, 8, 8, 8, 4, 4, 4, 4, 4, 8, 8, 8, 8, 8, 8, 4, 4, 8, 8, 4, 4, 4, 8, 8, 4, 4, 8, 8, 4, 8, 8, 8, 8, 8, 8, 4, 8, 8, 4]
+        play_time_pc= []
+        play_time_c = []
+        self.bpm_to_time (pre_chorous_type,play_time_pc)
+        self.bpm_to_time(chorous_type,play_time_c)
+
+        return pre_chorous_notes, chorous_notes, play_time_pc, play_time_c
+    
+    #equate note type to time
+    def bpm_to_time (self, music,array):
+        for i in music:
+            if i == 8:
+                t = .25
+            elif i == 4: 
+                t = .5
+            elif i == 2:
+                t = 1
+            elif i == 3:
+                t = 1.5
+            array.append(t)
+
 class Move():
     def __init__(self, perception):
         self.coordinate = {
