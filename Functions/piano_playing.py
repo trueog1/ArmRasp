57cf94ba5d7c089c3624fe859291fd91b34f09e8
Grace-S837 <shirotaa@oregonstate.edu>
2025-03-14 08:39:08 -0700

    5

--- Functions/piano_playing.py
+++ Functions/piano_playing.py
@@ -278,12 +278,12 @@ class Move():
 
                         #self.AK.setPitchRangeMoving((world_X, world_Y, 1.5), -90, -90, 0, 500)  #was originally 1000, maybe =1, so now should be a quarter of that?
                         self.AK.setPitchRangeMoving((world_X, world_Y, 10), -90, -90, 0, 400)
-                        time.sleep(0.15)
+                        time.sleep(0.25)
 
                         Board.setBusServoPulse(2, 10, 200)
                         #self.AK.setPitchRangeMoving((world_X, world_Y, 12), -90, -90, 0, 500)
                         self.AK.setPitchRangeMoving((world_X, world_Y, 15), -90, -90, 0, 400)  #机械臂抬起 = the robotic arm is raised
-                        time.sleep(0.15)
+                        time.sleep(0.25)
 
                         #self.initMove()  # 回到初始位置 = return to initial position
                         #time.sleep(0.75)
